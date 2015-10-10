2aa2cc91b336eec7ce8b45e3ddbe956810c5f20d
ZhangMin <xszhangmin@163.com>
2015-10-10 15:43:16 +0800

    1

---
+++ searchShow/src/main/java/com/miaozhen/withdata/api/consts/Consts.java
@@ -0,0 +1,10 @@
+package com.miaozhen.withdata.api.consts;
+
+import net.sf.json.JSONObject;
+
+public class Consts {
+
+	public static JSONObject JSON_FAIL = null;
+	public static JSONObject JSON_SUCCESS = null;
+
+}
